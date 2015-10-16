4607c445505f69367e737ee953498c12269e854b
Angelina <angelinaprod@gmail.com>
2015-10-16 14:50:14 -0700

    comments added

--- js/main.js
+++ js/main.js
@@ -67,6 +67,100 @@ TodoApp.renderListItem = function(todoItem) {
 	$edit.attr("type", "text");
 	$edit.addClass("edit");
 
+	//<button> - edit
+	var $editButton = $("<button>");
+	$editButton.attr("type", "button");
+	$editButton.addClass("btn btn-link btn-xs");
+	$editButton.text("Edit");
+
+	//edit todo
+	$editButton.on("click", function() {
+		$listItemSpan.hide();
+		$edit.show();
+		$edit.focus();
+		$edit.val($listItemSpan.text());
+		$edit.on ("blur", function() {
+			todoItem.name = $edit.val();
+			$edit.hide();
+			TodoApp.renderList();
+			TodoApp.save();
+		});
+		$edit.on('keydown', function(e){
+        	if(e.which == 13){
+            	this.blur();
+			}
+		});
+	});
+
+//---------------------------------------------
+
+	//<input> - comment
+	var $comment = $("<div>");
+	$comment.addClass("comment");
+	var $commentTextArea = $("<textarea>");
+	$commentTextArea.addClass("form-control");
+
+	//<button> - comment
+	var $commentButton = $("<button>");
+	$commentButton.attr("type", "button");
+	$commentButton.addClass("btn btn-link btn-xs");
+	$commentButton.text("Comment");
+
+	//<button> - save
+	var $saveButton = $("<button>");
+	$saveButton.attr("type", "button");
+	$saveButton.addClass("btn btn-info btn-xs commentButton");
+	$saveButton.text("Save");
+
+	//<button> - close
+	var $closeButton = $("<button>");
+	$closeButton.attr("type", "button");
+	$closeButton.addClass("btn btn-danger btn-xs commentButton");
+	$closeButton.text("Close");
+
+	//close comments
+	$closeButton.on ("click", function() {
+		$comment.hide();
+	})
+
+	//add comment
+	$commentButton.on("click", function() {
+		renderCommentList();
+		$comment.slideDown();
+	});
+		
+	$saveButton.on("click", function() {
+		addComment();
+		$commentTextArea.val(null);
+	});
+
+	
+	
+	var $commentList = $("<ul>");
+	$comment.append($commentList);
+
+
+
+	function addComment() {
+		var commentInput = $commentTextArea.val();
+		var newComment = new Comment(commentInput);
+		todoItem.comments.push(newComment);
+		renderCommentList();
+		TodoApp.save();
+	}
+
+	//clears list, loops through array, builds list item, appends to list
+	function renderCommentList() {
+		$commentList.empty();
+		for (var j = 0; j < todoItem.comments.length; j++) {
+			var eachItem = todoItem.comments[j].body;
+			var $item = $("<li>");
+			$item.addClass("commentListItem");
+			$item.text(eachItem);
+			$commentList.append($item);
+		}
+	}
+	
 	
 //---------------------------------------------
 
@@ -86,7 +180,6 @@ TodoApp.renderListItem = function(todoItem) {
 	});
 	
 	//strikethrough on checked
-
     if(todoItem.completed === true) {
         $listItem.addClass("striked");
         $checkbox.attr('checked', true);
@@ -97,7 +190,7 @@ TodoApp.renderListItem = function(todoItem) {
 	//<button> - delete
 	var $deleteButton = $("<button>");
 	$deleteButton.attr("type", "button");
-	$deleteButton.addClass("btn btn-default btn-xs");
+	$deleteButton.addClass("btn btn-link btn-xs");
 	$deleteButton.text("Delete");
 
 	//delete $listItem
@@ -113,39 +206,32 @@ TodoApp.renderListItem = function(todoItem) {
 
 	})
 
-//---------------------------------------------
-
-	//<button> - edit
-	var $editButton = $("<button>");
-	$editButton.attr("type", "button");
-	$editButton.addClass("btn btn-link btn-xs");
-	$editButton.text("Edit");
-
-	//edit todo
-	$editButton.on("click", function() {
-		$listItemSpan.hide();
-		$edit.show();
-		$edit.on ("blur", function() {
-			todoItem.name = $edit.val();
-			$edit.hide();
-			TodoApp.renderList();
-			TodoApp.save();
-		});
-		$edit.on('keydown', function(e){
-        	if(e.which == 13){
-            	this.blur();
-			}
-		});
-	});
 
 //---------------------------------------------
+	//wrap buttons for styling purposes
+	var $buttonWrapper = $("<div>");
+	$buttonWrapper
+		.addClass("button-wrapper")
+		.append($editButton)
+		.append($commentButton)
+		.append($deleteButton);
 
 	//append
-	$listItem.append($listItemSpan);
-	$listItem.append($edit);
-	$listItem.prepend($checkbox);
-	$listItem.append($deleteButton);
-	$listItem.append($editButton);
+	$listItem
+		.append($listItemSpan)
+		.append($edit)
+		.prepend($checkbox)
+		.append($buttonWrapper);
+
+	$comment
+		.append($commentTextArea)
+		.append($saveButton)
+		.append($closeButton);
+	
+	
+	$listItem.append($comment);
+	
+
 
 	return $listItem;
 }
@@ -154,6 +240,12 @@ TodoApp.renderListItem = function(todoItem) {
 function TodoItem(name) {
 	this.name = name;
 	this.completed = false;	
+	this.comments = [];
+}
+
+function Comment(body) {
+	this.from = "Angelina";
+	this.body = body;
 }
 
 
@@ -185,15 +277,49 @@ TodoApp.init();
 //			 PERSONAL NOTES
 //---------------------------------------------
 
+//comments
+	//add comment button
+		//when clicked inside of the li you will side down (maybe in a div) a text area and a save button
+			//built into the li. hidden with display none
+		//when hit save it will hide section and create comment on todo
+			//still be an embedded list inside of li
+		//see below for details - just not in a model yet
+		//when you build the area that slides down wrap in div
+		//just like todo list - build it and loop through and render
+
+		//1 build div that has text area and button and append to li
+		//2 when hit add comment it slides it down
+		//3 when type in text area and hit button it creates new instance of comment consturctor
+			//push to comments array for that todo item
+			//console log to see if it works
+		//contact jono for further instructions
+
+	// create add comment button next to edit button
+	// inside of that pop open a model (bootstrap)
+	// once inside of the popup it will have a text area (body) and save button
+	// when you click save you are going to close the model and create a new comment instance 
+	// comment instance will be pushed into the array
+	//what you see now is under the li there is another ul inside of each li
+	//in each li there is a ul thats appended at the bottom with a list of all the comments
+	//when you loop through and build each li you are now appending a ul as well.
+
+	//very last thing -- accordian the comments so they aren't always visible
+
+	//play with bootstrap model first
+		//can either do no javascript and just attr on html element
+		//or progromatically 
+		//shared model
+		//when click you show - already there but display none
+			//find a way to pass an item to the model
 
 //create edit button inside of <li>
-	//create input inside of <li> that is display none
-//when edit button clicked
-	//hide text and show input
-	//***wrap text in span or div so you can hide it
-//on blur change back so input is hidden
-	//update item name and render list
-	//***don't need to "reshow" the text because of render list
+		//create input inside of <li> that is display none
+	//when edit button clicked
+		//hide text and show input
+		//***wrap text in span or div so you can hide it
+	//on blur change back so input is hidden
+		//update item name and render list
+		//***don't need to "reshow" the text because of render list
 
 //add delete button in renderListItem
 	//when delete button clicked
